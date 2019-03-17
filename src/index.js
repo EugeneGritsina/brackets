c1792d309aa097c7ae1bf37e684fe1653ef843c3
Eugene Gritsina <eugene.gritsina@gmail.com>
2019-03-18 02:47:27 +0300

    13 passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0)
+    return false;
+  debugger;
+  var pairs = new Array(str.length / 2);
+  for(var i = 0; i < pairs.length; i++){
+    pairs[i] = new Array(2);
+  }
+
+  var iteratorPairs = 0;
+
+  for (var i=0; i < str.length; i++) {
+    for (var j=0; j < bracketsConfig.length; j++)
+      if (str[i] == bracketsConfig[j]) {
+        if (j % 2 != 0) {  // для закрывающих скобок
+          if (pairs[iteratorPairs][0] == 0) //если текущая пара скобок начинается с закрывающей => конец
+            return false;
+          pairs[iteratorPairs][1]++;    //заполнение этой пары флагом для закрывающей скобки
+          iteratorPairs++; // переход в следующую ячейку для пар
+        }
+        else {
+          if (!iteratorPairs) { // для первого вхождения
+            pairs[iteratorPairs][0]++;
+            continue;
+          }
+          if (pairs[iteratorPairs][0] == 1 && pairs[iteratorPairs][1] == 1) { //проверка на заполненность последней пары
+            iteratorPairs++;
+            pairs[iteratorPairs][0]++;
+          }
+          else
+            return false;
+        }
+      }
+  }
+
+  return true;
 }
